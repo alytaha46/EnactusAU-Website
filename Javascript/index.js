3c2dd1aecd462683042b9028ba01bacd7f0598a0
Aly Taha <47082177+alytaha46@users.noreply.github.com>
2020-10-28 21:17:28 +0200

    some saves

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,4 +1,11 @@
 //GO TO UP BUTTON
+$(document).ready(function () {
+
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });  
+  });
 $(function () {
     // previous detection logic
 
