c5a18800692ad30e9177b9d9183b34d3a316f3b2
Aly Taha <47082177+alytaha46@users.noreply.github.com>
2020-10-29 15:40:41 +0200

    some final finishes

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,21 +1,15 @@
-//GO TO UP BUTTON
 $(document).ready(function () {
-
     $('.counter').counterUp({
       delay: 10,
       time: 1000
-  });  
-  });
-$(function () {
-    // previous detection logic
+    });
 
     $("#to-top").on("click", function () {
         $("html, body").animate({
             scrollTop: 0
         }, 200);
     });
-});
-$(function () {
+
     $(window).scroll(function () {
         if ($(this).scrollTop() - 200 > 0) {
             $('#to-top').stop().slideDown('fast'); // show the button
@@ -23,6 +17,7 @@ $(function () {
             $('#to-top').stop().slideUp('fast'); // hide the button
         }
     });
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
             $("#navigation-bar").addClass("fixed");
@@ -31,5 +26,6 @@ $(function () {
             $("#navigation-bar").removeClass("fixed");
         }
     });
-});
+
+  });
 
--- Javascript/join-us.js
+++ Javascript/join-us.js
@@ -1,25 +1,26 @@
-$(function () {
-    // previous detection logic
-    $("#to-top").on("click", function () {
+$(document).ready(function () {
+    $(function () {
+        // previous detection logic
+        $("#to-top").on("click", function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 200);
+        });
+    });
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() - 200 > 0) {
+                $('#to-top').stop().slideDown('fast'); // show the button
+            } else {
+                $('#to-top').stop().slideUp('fast'); // hide the button
+            }
+        });
+    });
+    $("#frame").on( "load", function() {
         $("html, body").animate({
             scrollTop: 0
-        }, 200);
+        }, 500);
     });
 });
-$(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() - 200 > 0) {
-            $('#to-top').stop().slideDown('fast'); // show the button
-        } else {
-            $('#to-top').stop().slideUp('fast'); // hide the button
-        }
-    });
-    
-});
-function resizeIframe(obj) {
-    $("html, body").animate({
-        scrollTop: 0
-    }, 200);
-  }
 
 
